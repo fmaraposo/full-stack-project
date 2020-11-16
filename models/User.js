a7ae5fc00f56c0db6b1fb4585182b070d553ffbe
anac-jeronimo <anac_jeronimo@hotmail.com>
2020-11-16 12:17:53 +0000

    forms

--- models/User.js
+++ models/User.js
@@ -17,7 +17,10 @@ const userSchema = new Schema (
         password: {
             type:String,
             required: [true, "Password is required"]
-            }
+        }, 
+        imageUrl: {
+            type: String
+        }
     }   
 );
 
