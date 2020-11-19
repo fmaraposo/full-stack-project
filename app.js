4b91d902d7885f970393f90efaacf1d331c350b0
anac-jeronimo <anac_jeronimo@hotmail.com>
2020-11-19 18:25:25 +0000

    styles

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Placeholder title';
+app.locals.title = 'Portuguese Expressions';
 
 
 
